f04fe7a7a38fd90efa53eb4a0dd45edffd52fc65
Ayshen2006 <13561@holbertonstudents.com>
2026-03-21 08:32:45 +0000

    It is okayy

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,8 @@ It is used to demonstrate test-driven development in Python.
 All functions handle integers and floats with proper type checking.
 """
 def add_integer(a, b=98):
-    """Adds two integers and returns the result."""
+    """Adds two integers and returns the result.
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
