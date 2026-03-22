87d0dc6640529fbab77abef6b88d6895126d84d1
Ayshen2006 <13561@holbertonstudents.com>
2026-03-22 09:22:19 +0000

    float and float pos is changed

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-number = 3.14
-print(f"Float: {number}")
+number = 3.14568
+print("Float: {:.2f}".format(number))
 
--- python-hello_world/4-print_float_pos.py
+++ python-hello_world/4-print_float_pos.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-number = 333.12
-print(f"Float: {number}")
+number = 333.123456
+print("Float: {:.2f}".format(number))
+
 
