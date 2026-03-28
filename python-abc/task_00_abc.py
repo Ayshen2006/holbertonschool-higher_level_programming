873463b99ee28bb33074c0e1d5c70ad87761fea7
Ayshen2006 <13561@holbertonstudents.com>
2026-03-28 13:23:43 +0000

    task_00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+class Animal(ABC):
+	@abstractmethod
+	def sound(self):
+	   pass
+class Dog(Animal):
+	def sound(self):
+		print("Bark")
+class Cat(Animal):
+	def sound(self):
+		print("Meow")
+
