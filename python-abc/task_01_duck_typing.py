746b4960989f02bff3968e9fbbef6b0efc6467cb
Ayshen2006 <13561@holbertonstudents.com>
2026-03-28 16:24:21 +0000

    ok

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -12,9 +12,9 @@ class circle(shape):
     def __init__(self,radius):
         self.radius=radius
     def area(self):
-                return pi*self.radius**2
+                return math.pi*self.radius**2
     def perimeter(self):
-                return 2*pi*self.radius
+                return 2*math.pi*self.radius
 class rectangle(shape):
     def __init__(self,width,height):
         self.width=width
