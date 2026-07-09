b0cd42bdf9f98abf871f52e9bec98eb2929b9fcc
Ayshen2006 <13561@holbertonstudents.com>
2026-07-09 15:36:53 +0400

    Fixedd

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -4,7 +4,11 @@ print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
-print(add_integer(-1, -1))
-print(add_integer(4, 5.7))
-print(add_integer(4, "School"))
-print(add_integer(None))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
